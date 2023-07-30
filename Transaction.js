12848732788e2a9aabe3698d85737f265d8c70cd
TinyTinu <139676301+TinyTinu@users.noreply.github.com>
2023-07-30 22:48:08 +0530

    UpdateTransaction.js

--- Transaction.js
+++ Transaction.js
@@ -1,3 +1,4 @@
+//Check one
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx').Transaction;
 
@@ -124,6 +125,75 @@ async function transferTokens() {
     // Transfer tokens from the token contract to the admin address
     const transferReceipt = await tokenContract.methods.transfer(adminAddress, amountToSend).send({ from: tokenContractAddress });
     console.log('Transfer receipt:', transferReceipt);
+  } catch (err) {
+    console.error('Error transferring tokens:', err);
+  }
+}
+
+transferTokens();
+
+
+//Check 3
+const Web3 = require('web3');
+const Tx = require('ethereumjs-tx').Transaction;
+
+// Web3 provider for the desired network (e.g., Infura)
+const web3Provider = 'https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY';
+const web3 = new Web3(new Web3.providers.HttpProvider(web3Provider));
+
+// Sender's account address and private key
+const senderAddress = '0x...'; // Replace with the sender's Ethereum address
+const privateKey = Buffer.from('PRIVATE_KEY', 'hex'); // Replace with the sender's private key
+
+// ERC-20 token contract address and ABI
+const tokenContractAddress = '0x...'; // Replace with the ERC-20 token contract address
+const tokenABI = [/* ... */]; // Replace with the ERC-20 token contract ABI
+
+// Array of user addresses to transfer tokens to
+const userAddresses = ['0x...', '0x...', '0x...', /* Add more user addresses here */];
+
+// Number of tokens to transfer per user (in the smallest unit of the token, e.g., 18 decimals for DAI, USDT)
+const amountToSend = '1000000000000000000'; // Replace with the desired amount
+
+async function transferTokens() {
+  try {
+    // ERC-20 token contract instance
+    const tokenContract = new web3.eth.Contract(tokenABI, tokenContractAddress);
+
+    // Get the transaction count of the sender's address
+    const nonce = await web3.eth.getTransactionCount(senderAddress, 'pending');
+
+    // Prepare the data to call the transfer function on the token contract
+    const data = tokenContract.methods.transfer('PLACEHOLDER_ADDRESS', amountToSend).encodeABI();
+
+    // Get the gas price
+    const gasPrice = await web3.eth.getGasPrice();
+
+    for (const userAddress of userAddresses) {
+      // Replace the 'PLACEHOLDER_ADDRESS' in the data with the current user address
+      const transferData = data.replace('PLACEHOLDER_ADDRESS', userAddress);
+
+      // Create the transaction object
+      const transactionObject = {
+        from: senderAddress,
+        to: tokenContractAddress,
+        gasPrice: web3.utils.toHex(gasPrice),
+        gasLimit: web3.utils.toHex(80000), // Replace with an appropriate gas limit
+        nonce: web3.utils.toHex(nonce),
+        data: transferData,
+      };
+
+      // Sign the transaction
+      const tx = new Tx(transactionObject, { 'chain': 'mainnet' }); // Use 'rinkeby' for the Rinkeby testnet
+      tx.sign(privateKey);
+
+      // Serialize the signed transaction
+      const serializedTx = tx.serialize();
+
+      // Send the transaction to the Ethereum network
+      const receipt = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
+      console.log(`Tokens transferred to ${userAddress}. Transaction hash:`, receipt.transactionHash);
+    }
   } catch (err) {
     console.error('Error transferring tokens:', err);
   }
